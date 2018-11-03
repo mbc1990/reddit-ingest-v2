488095534dc09fdedeee5a4e2e4b5ac5d97285bc
mbc <malcolmbalch@gmail.com>
2018-11-03 16:42:36 -0400

    Not compiling yet

--- reddt-ingest/src/main.rs
+++ reddt-ingest/src/main.rs
@@ -3,10 +3,15 @@ extern crate serde_derive;
 extern crate serde;
 extern crate toml;
 extern crate reqwest;
+extern crate threadpool;
 
 use std::fs::File;
 use std::env;
 use std::io::Read;
+use std::sync::mpsc::channel;
+
+use threadpool::ThreadPool;
+
 
 mod reddit_client;
 mod config;
@@ -28,7 +33,16 @@ fn main() {
     let subreddits = decoded.subreddits.clone();
     let rc = reddit_client::RedditClient::new(decoded);
 
-    let needle = "soros funded";
+    // TODO: A single thread that periodically adds subreddits to the queue so we continue updating
+
+    // TODO: A worker pool that reads from a queue, making subreddit or comment requests and doing string comparisons (for now)
+    let (tx, rx) = channel();
+    let n_workers = 4;
+    let pool = ThreadPool::new(n_workers);
+
+    // TODO: Second worker pool that does the string comparisons
+
+    let needle = "liberal";
 
     for subreddit in subreddits.iter() {
         println!("{:?}", subreddit);
@@ -36,28 +50,38 @@ fn main() {
         let resp = rc.do_authenticated_request(api_query).unwrap();
         let stories = resp["data"]["children"].as_array().unwrap();
         let mut total_comments = 0;
-        for story in stories.iter() {
-            // Sort by new since this is supposed to be semi-real time
-            let permalink = &story["data"]["permalink"];
-            let comments_query = &[permalink.as_str().unwrap(), "?sort=new"].concat();
+        for their_story in stories.iter() {
+            let story = their_story.clone();
+            pool.execute(move|| {
+                // Sort by new since this is supposed to be semi-real time
+                let permalink = &story["data"]["permalink"];
+                let comments_query = &[permalink.as_str().unwrap(), "?sort=new"].concat();
 
-            let comments = rc.do_authenticated_request(comments_query).unwrap();
-            for entry in comments.as_array().unwrap().iter() {
-                let mut comments_for_story = 0;
-                let raw_comments = rc.parse_comment_tree(&entry);
-                for comment in raw_comments.iter() {
-                    if comment.contains(needle) {
-                        println!("{:?}", comment)
+                let comments = rc.do_authenticated_request(comments_query).unwrap();
+                for entry in comments.as_array().unwrap().iter() {
+                    let mut comments_for_story = 0;
+                    let raw_comments = rc.parse_comment_tree(&entry);
+                    for comment in raw_comments.iter() {
+                        if comment.contains(needle) {
+                            println!("{:?}", comment.as_str());
+
+                            // TODO: This will need to work at some point
+                            tx.send(comment.to_owned());
+                        }
+                        // TODO: This probaly won't work
+                        total_comments += 1;
+                        comments_for_story += 1;
                     }
-                    total_comments += 1;
-                    comments_for_story += 1;
+                    // The first entry has no comments? TODO: Confirm the api structure
+                    if comments_for_story > 1 {
+                        println!("{:?} total comments for story: {:?}", comments_for_story, permalink);
+                    }
                 }
-                // The first entry has no comments? TODO: Confirm the api structure
-                if comments_for_story > 1 {
-                    println!("{:?} total comments for story: {:?}", comments_for_story, permalink);
-                }
-            }
+            });
         }
         println!("{:?} total comments for {:?}", total_comments, subreddit);
+    }
+    for output in rx.iter() {
+        println!("Output: {:?} ", output);
     }
 }
